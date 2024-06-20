e1ddb4a6d41283107b7361dd4a0247c1ecd0e89d
Simeon Mihaylov <mihaylov.design@gmail.com>
2024-06-20 14:32:16 +0300

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+const apiUrl = "/choreo-apis/pythonnotesapp/backend/v1";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 api.interceptors.request.use(
